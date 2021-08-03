f429ffea0dc1dfaf56b263e9dec3510df2216363
Olya Kikot <kikotolya1989@gmail.com>
2021-08-03 11:41:07 +0300

    color-swith

---
+++ main.js
@@ -0,0 +1,38 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+
+ const refs ={
+     btnStart: document.querySelector('button[data-action="start"]'),
+     btnStop: document.querySelector('button[data-action="stop"]'),
+     body: document.querySelector('body'),
+ } 
+ let idStart = null;
+ refs.btnStart.addEventListener('click', onClickBtnStart);
+ refs.btnStop.addEventListener('click', onClickBtnStop);
+
+ function onClickBtnStart (){
+
+   idStart = setInterval(() =>{
+    
+        refs.body.style.backgroundColor= colors[randomIntegerFromInterval(0, colors.length)];
+    },1000)
+    
+    refs.btnStart.disabled = true;
+
+ }
+
+function onClickBtnStop (){
+   refs.btnStart.disabled = false;
+   clearInterval(idStart);
+}
+
+ const randomIntegerFromInterval = (min, max) => {
+    return (Math.floor(Math.random() * (max - min + 1) + min));
+  };
